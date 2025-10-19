62a26a1ee05aac21790c0409618c84f92ddc9db2
sclolus <sclolus@sclolus-debian>
2025-10-19 16:11:16 +0200

    Added cli args + plotting

--- src/bin/trainer.rs
+++ src/bin/trainer.rs
@@ -1,6 +1,6 @@
 use pest::Parser;
 use pest_derive::Parser;
-use clap;
+use clap::Parser as ClapParser;
 
 #[derive(Parser)]
 #[grammar = "./src/grammars/data.pest"]
@@ -13,17 +13,76 @@ struct Entry {
 }
 
 
-#[derive(clap::Parser)]
+#[derive(ClapParser, Debug)]
 #[command(name = "trainer")]
 #[command(about = "Trains the linear regression model on the data.csv file", long_about = None)]
 struct Cli {
-	#[arg(long)]
-	plot_data_only: boolean,
+	#[arg(long, default_value_t = false)]
+	plot_data_only: bool,
+	
+	#[arg(short, long, default_value_t = false)]
+	plot: bool,
+	
 	#[arg(short, long, default_value_t = 0.1)]
 	learning_rate: f64,
 }
 
+struct PlotArgs<'a> {
+	plot_data_only: bool,
+	theta0: f64,
+	theta1: f64,
+	data_points: &'a Vec<Entry>,
+}
+
+use plotters::prelude::*;
+
+fn generate_plot(args: PlotArgs) -> Result<(), Box<dyn std::error::Error>> {
+	let root = BitMapBackend::new("plot.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+	let min_price = args.data_points.iter().map(|x| x.price).fold(f64::INFINITY, f64::min);
+	let max_price = args.data_points.iter().map(|x| x.price).fold(f64::NEG_INFINITY, f64::max);
+
+	let min_mileage = args.data_points.iter().map(|x| x.mileage).fold(f64::INFINITY, f64::min);
+	let max_mileage = args.data_points.iter().map(|x| x.mileage).fold(f64::NEG_INFINITY, f64::max);
+    
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Mileage-price linear regression ", ("sans-serif", 50))
+        .margin(5)
+        .x_label_area_size(64)
+        .y_label_area_size(64)
+        .build_cartesian_2d(min_mileage..max_mileage, min_price..max_price)?;
+    
+    chart.configure_mesh().draw()?;
+    
+    chart.draw_series(PointSeries::of_element(
+        args.data_points.iter().map(|point| (point.mileage, point.price)),
+		5,
+        &RED,
+		&|coord, size, style| {
+			EmptyElement::at(coord) + Cross::new((0, 0), size, style)
+		}
+    ))?;
+
+	if !args.plot_data_only {
+		let theta0 = args.theta0;
+		let theta1 = args.theta1;
+			
+		chart.draw_series(LineSeries::new(
+			vec![(min_mileage, theta0 + theta1 * min_mileage),
+			(max_mileage, theta0 + theta1 * max_mileage)],
+			&GREEN,
+		))?;
+	}
+    
+    root.present()?;
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let args = Cli::parse();
+	
+	
     let file_contents =
         std::fs::read_to_string("data.csv").or(Err("Failed to read data.csv file"))?;
     let pairs = DataParser::parse(Rule::csv_file, &file_contents)
@@ -31,14 +90,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut entries: Vec<Entry> = Vec::with_capacity(128);
     for pair in pairs.clone() {
-        println!("-{:?}: {}", pair.as_rule(), pair.as_str());
+        // println!("-{:?}: {}", pair.as_rule(), pair.as_str());
         match pair.as_rule() {
             Rule::record => {
                 let mut price = 0.0;
                 let mut mileage = 0.0;
 
                 for pair in pair.into_inner() {
-					println!("--{:?}: {}", pair.as_rule(), pair.as_str());
+					// println!("--{:?}: {}", pair.as_rule(), pair.as_str());
                     match pair.as_rule() {
                         Rule::mileage => {
                             price = pair
@@ -63,11 +122,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-	let learning_rate = 0.5;		// Make this configurable?
+	let learning_rate = args.learning_rate;		// Make this configurable?
 
-	let mut theta0 = 0.0;
-	let mut theta1 = 0.0;
+	let mut theta0 = 10000.0;
+	let mut theta1 = -0.1;
 		
 
+	println!("{:?}", args);
+	println!("{:?}", entries);
+	// let plot_data_only: bool = args.plot_data_only;
+	let plot_data_only: bool = true;
+	let plot: bool = args.plot;
+		
+	if plot_data_only || plot {
+		generate_plot(PlotArgs {
+			plot_data_only,
+			theta0,
+			theta1,
+			data_points: &entries,
+		})?
+	}
     Ok(())
 }
